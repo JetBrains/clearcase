e3e8cbcde8ec6c3ebd96c396cc8a7971f31464ed
Michael Gerasimov <lloix@intellij.com>
2007-02-14 19:18:24 +0300

    IDEADEV-13223, rebase in the main menu.

---
+++ src/net/sourceforge/transparent/actions/RebaseAction.java
@@ -0,0 +1,17 @@
+package net.sourceforge.transparent.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+
+public class RebaseAction extends AsynchronousAction
+{
+  @NonNls private final static String ACTION_NAME = "Rebase Project";
+
+  public void perform( VirtualFile file, AnActionEvent e )
+  {
+    cleartool( "rebase", "-g" );
+  }
+
+  protected String getActionName() {  return ACTION_NAME;  }
+}
