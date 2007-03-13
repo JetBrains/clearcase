ffbb95d9beb35b5a9ef03863592a9bf653f23431
Michael Gerasimov <lloix@intellij.com>
2007-03-13 15:28:51 +0300

    IDEADEV-15185, forbid command "Properties" for a hijacked file.

--- src/net/sourceforge/transparent/actions/PropertiesAction.java
+++ src/net/sourceforge/transparent/actions/PropertiesAction.java
@@ -1,12 +1,30 @@
 package net.sourceforge.transparent.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.FileStatus;
+import com.intellij.openapi.vcs.FileStatusManager;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.vcsUtil.VcsUtil;
 import org.jetbrains.annotations.NonNls;
 
 public class PropertiesAction extends AsynchronousAction
 {
   @NonNls private final static String ACTION_NAME = "Properties";
+
+  public void update( AnActionEvent e )
+  {
+    super.update( e );
+    VirtualFile[] files = VcsUtil.getVirtualFiles( e );
+
+    if( e.getPresentation().isEnabled())
+    {
+      Project project = e.getData( DataKeys.PROJECT );
+      FileStatusManager mgr = FileStatusManager.getInstance( project );
+      e.getPresentation().setEnabled( mgr.getStatus( files[ 0 ] ) != FileStatus.HIJACKED );
+    }
+  }
 
   public void perform( VirtualFile file, AnActionEvent e ) {
     cleartool( "describe", "-g", file.getPath() );
