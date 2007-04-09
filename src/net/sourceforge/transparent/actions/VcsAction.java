1feba39ded6c62c09ca7554058282c8df4417f2b
Michael Gerasimov <lloix@intellij.com>
2007-04-09 13:38:19 +0400

    substitute inproper usage of Updated files with AbstractVcsHelper.

--- src/net/sourceforge/transparent/actions/VcsAction.java
+++ src/net/sourceforge/transparent/actions/VcsAction.java
@@ -9,11 +9,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vcs.AbstractVcsHelper;
 import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.openapi.vcs.update.FileGroup;
-import com.intellij.openapi.vcs.update.UpdatedFiles;
 import com.intellij.openapi.vfs.VirtualFile;
 import net.sourceforge.transparent.TransparentVcs;
 import org.jetbrains.annotations.NonNls;
@@ -23,10 +20,7 @@ import java.util.List;
 
 public abstract class VcsAction extends AnAction
 {
-  @NonNls private static final String GROUP_NAME_ID = "OPERATION_FAIL";
-  @NonNls private static final String GROUP_TITLE = "Operation Failed";
   @NonNls private static final String OPERATION_FAILED_TEXT = "One or more errors occured during operation";
-  @NonNls private static final String TITLE = "Operation Failed on Files:";
 
   public void update( AnActionEvent e )
   {
@@ -48,16 +42,9 @@ public abstract class VcsAction extends AnAction
 
     if( errors.size() > 0 )
     {
-      UpdatedFiles updatedFiles = UpdatedFiles.create();
-      updatedFiles.registerGroup( new FileGroup( GROUP_TITLE, GROUP_TITLE, false, GROUP_NAME_ID, true ));
-
-      for( VcsException exc : errors )
-      {
-        updatedFiles.getGroupById( GROUP_NAME_ID ).add( exc.getVirtualFile().getPath() );
-      }
-
-      ProjectLevelVcsManager.getInstance( project ).showProjectOperationInfo( updatedFiles, TITLE );
-      Messages.showErrorDialog( project, OPERATION_FAILED_TEXT, GROUP_TITLE );
+      @NonNls final String title = getActionName( e ) + " failed";
+      AbstractVcsHelper.getInstance( project ).showErrors( errors, title );
+      Messages.showErrorDialog( project, OPERATION_FAILED_TEXT, title );
     }
   }
 
