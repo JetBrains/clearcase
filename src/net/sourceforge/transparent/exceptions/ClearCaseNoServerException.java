e08b000bc93444b6de7996edcdfda0ebe21c5101
Alexey Kudravtsev <cdr@intellij.com>
2017-05-10 19:40:29 +0300

    removed default file header, because it adds no information but subtracts attention and space

--- src/net/sourceforge/transparent/exceptions/ClearCaseNoServerException.java
+++ src/net/sourceforge/transparent/exceptions/ClearCaseNoServerException.java
@@ -1,9 +1,5 @@
 package net.sourceforge.transparent.exceptions;
 
-/**
- * Created by IntelliJ IDEA.
- * User: lloix
- */
 public class ClearCaseNoServerException extends ClearCaseException {
   public ClearCaseNoServerException( String error ) {
     super( error );
