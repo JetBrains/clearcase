05519344a14f1dc5218549e954e29970e8260717
Alexey Kudravtsev <cdr@intellij.com>
2017-09-14 15:58:27 +0300

    override

--- src/net/sourceforge/transparent/ContentRevisionFactory.java
+++ src/net/sourceforge/transparent/ContentRevisionFactory.java
@@ -50,13 +50,15 @@ public class ContentRevisionFactory
   private static class VFSKeysListener implements VirtualFileListener {
     public VFSKeysListener() {}
 
-    public void beforeFileMovement( @NotNull VirtualFileMoveEvent e )
+    @Override
+    public void beforeFileMovement(@NotNull VirtualFileMoveEvent e )
     {
       String oldPath = e.getOldParent().getPath() + "/" + e.getFileName();
       analyzeEvent( oldPath );
     }
 
-    public void beforePropertyChange( @NotNull VirtualFilePropertyEvent e )
+    @Override
+    public void beforePropertyChange(@NotNull VirtualFilePropertyEvent e )
     {
       final VirtualFile file = e.getFile();
       if( e.getPropertyName() == VirtualFile.PROP_NAME )
